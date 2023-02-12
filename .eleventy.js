3ae7157711d3f77cf37d74b3cb5550cf9f9a7000
Lea Rosema <lea@lea.codes>
2023-02-13 00:12:04 +0100

    chore: work on config

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,80 @@
-const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight');
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const { EleventyRenderPlugin } = require('@11ty/eleventy');
 
-const htmlMinTransform = require('./eleventy/html-min-transform');
-const filterGlslMinify = require('./eleventy/filter-glslminify');
+const esbuildPlugin = require('./config/plugins/esbuild');
+const postCSSPlugin = require('./config/plugins/postcss');
+const htmlTransformPlugin = require('./config/plugins/html-transform');
+const {
+  formatDate,
+  isoDate,
+  limit,
+  minify,
+  where,
+  splitLines,
+  slugify,
+} = require('./config/filters/index');
 
-module.exports = (config) => {
-  config.addPlugin(pluginSyntaxHighlight);
-  config.addPlugin(pluginRss, {
-    posthtmlRenderOptions: {},
+// module import events
+const { svgToJpeg } = require('./config/events/index.js');
+
+module.exports = (eleventyConfig) => {
+  // custom watch targets
+  eleventyConfig.addWatchTarget('./src/assets');
+
+  // plugins
+  eleventyConfig.addPlugin(syntaxHighlightPlugin);
+  eleventyConfig.addPlugin(rssPlugin);
+  eleventyConfig.addPlugin(esbuildPlugin);
+  eleventyConfig.addPlugin(postCSSPlugin);
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(htmlTransformPlugin);
+
+  // filters
+  eleventyConfig.addFilter('formatDate', formatDate);
+  eleventyConfig.addFilter('isoDate', isoDate);
+  eleventyConfig.addFilter('limit', limit);
+  eleventyConfig.addFilter('minify', minify);
+  eleventyConfig.addFilter('where', where);
+  eleventyConfig.addFilter('splitLines', splitLines);
+  eleventyConfig.addFilter('slugify', slugify);
+  eleventyConfig.addFilter('keys', Object.keys);
+  eleventyConfig.addFilter('values', Object.values);
+  eleventyConfig.addFilter('entries', Object.entries);
+
+  // short codes
+  eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`); // current year, stephanie eckles
+  eleventyConfig.addShortcode('packageVersion', () => `v${packageVersion}`);
+
+  // passthrough copy
+  // same path
+  ['src/assets/fonts/', 'src/assets/images/'].forEach((path) =>
+    eleventyConfig.addPassthroughCopy(path)
+  );
+
+  // social icons to root directory
+  eleventyConfig.addPassthroughCopy({
+    'src/assets/images/favicon/*': '/',
   });
-  config.addTransform('htmlmin', htmlMinTransform);
+
+  eleventyConfig.addPassthroughCopy({
+    'src/assets/css/global.css': 'src/_includes/global.css',
+  });
+
+  // build events
+  eleventyConfig.on('afterBuild', svgToJpeg);
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
-  config.setUseGitIgnore(false);
-
-  // Custom filters
-  config.addFilter('glslminify', filterGlslMinify);
+  eleventyConfig.setUseGitIgnore(false);
 
   return {
     markdownTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dir: {
+      output: 'dist',
       input: 'src',
-      output: 'public',
+      includes: '_includes',
+      layouts: '_layouts',
     },
-    templateFormats: ['njk', 'md', '11ty.js'],
   };
 };
---
+++ config/filters/index.js
@@ -0,0 +1,17 @@
+const formatDate = require('./formatdate');
+const isoDate = require('./isodate');
+const limit = require('./limit');
+const minify = require('./minify');
+const where = require('./where');
+const splitLines = require('./splitlines');
+const slugify = require('./slugify');
+
+module.exports = {
+  formatDate,
+  isoDate,
+  limit,
+  minify,
+  where,
+  splitLines,
+  slugify,
+};
